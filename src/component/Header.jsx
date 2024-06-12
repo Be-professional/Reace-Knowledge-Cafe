644b07c0886aeb2efcdce0bac5e7df3ceacd67e9
Your New Name <your-new-email@example.com>
2024-06-12 05:46:45 -0700

    header update

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,10 @@
+import pic from '../assets/images/profile.png'
 
 const Header = () => {
     return (
         <div>
           <h1 className='text-4xl font-bold'>React Knowledge Cafe</h1>
+          <img src={pic} alt="" />
 
         </div>
     );
