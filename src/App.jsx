8085b25e3f2ba294d5f506ec538f680f730732a6
Your New Name <your-new-email@example.com>
2024-06-12 05:27:41 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Blogs from "./component/Blogs"
+import Header from "./component/Header"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
